7c7977750ff313fd98c92ce5e91d3cebf8b85c53
Glenn Galicia <glenn.f.g.a@gmail.com>
2023-08-07 00:07:13 -0600

    accesing an values object

---
+++ 07-Objetos/js/02-app.js
@@ -0,0 +1,10 @@
+//Object Literal
+const producto = {
+    nombre: "Monitor Samsung 4K",
+    precio: 300,
+    disponible: true
+}
+
+console.log(producto.nombre)
+console.log(producto.precio)
+console.log(producto['disponible'])
