5b8f96ffb8c0f0bf8aa34e67f776659303ccdfc1
Glenn Galicia <glenn.f.g.a@gmail.com>
2023-08-08 00:08:00 -0600

    keys, values, entries

---
+++ 07-Objetos/js/13-app.js
@@ -0,0 +1,14 @@
+
+// Object Literal
+const producto = {
+    nombre: "Monitor Samsung 4K",
+    precio: 300,
+    disponible: true,
+   
+}
+
+console.log(Object.keys(producto))
+
+console.log(Object.values(producto))
+
+console.log(Object.entries(producto))
