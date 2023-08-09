329593d6f430c62cd084609f3b8e3eca0037ae00
Glenn Galicia <glenn.f.g.a@gmail.com>
2023-08-08 21:52:20 -0600

    array declaration

---
+++ 08-Arrays/js/01-app.js
@@ -0,0 +1,8 @@
+const numeros = [10, 20, 30, 40]
+const meses = new Array('Enero','Febrero','Marzo')
+
+console.log(numeros)
+console.log(meses)
+
+const deTodo = ['Hola',{nombre:'Glenn'},10, null]
+console.log(deTodo)
