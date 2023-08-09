616006c3ac0d1996f1abf47046ea2f9fa155976b
Glenn Galicia <glenn.f.g.a@gmail.com>
2023-08-09 14:35:20 -0600

    params and arguments in functions

---
+++ 09-Funciones/js/05-app.js
@@ -0,0 +1,11 @@
+function sumar(a, b) { // parametros
+    console.log(a + b)
+}
+
+sumar(10, 45) // argumentos
+
+function saludar(nombre, apellido){
+    console.log(`Hola ${nombre} ${apellido}`)
+}
+
+saludar('Glenn', 'Galicia')
