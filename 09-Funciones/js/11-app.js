70d3fd47e3c33165cabaa8010d15ca33c684e698
Glenn Galicia <glenn.f.g.a@gmail.com>
2023-08-09 23:01:47 -0600

    advantages of arrow function

---
+++ 09-Funciones/js/11-app.js
@@ -0,0 +1,7 @@
+const aprendiendo = function(tecnologia){
+    console.log(`Aprendiendo ${tecnologia}`)
+}
+aprendiendo('JS')
+
+const aprendiendo2 = (tecnologia) => `Aprendiendo ${tecnologia}`
+console.log(aprendiendo2('Math'))
