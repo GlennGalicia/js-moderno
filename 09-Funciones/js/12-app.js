e92d18e45f3af284023504e4e49608c5d0e73dfd
Glenn Galicia <glenn.f.g.a@gmail.com>
2023-08-09 23:09:27 -0600

    arrow function into foreach and map

---
+++ 09-Funciones/js/12-app.js
@@ -0,0 +1,10 @@
+const carrito = [
+    { nombre: 'Monitor 4k', precio: 4800 },
+    { nombre: 'Teclado touch', precio: 5000 },
+    { nombre: 'Iphone 13 mini', precio: 18500 }
+]
+
+const nuevoArray = carrito.map(producto => `El producto ${producto.nombre} tiene precio de ${producto.precio}`)
+console.log(nuevoArray)
+
+carrito.forEach(producto => console.log(`El producto ${producto.nombre} tiene precio de ${producto.precio}`))
