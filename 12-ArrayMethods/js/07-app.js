046809a2100636da7c4ce7f54ae294db7ddec170
Glenn Galicia <glenn.f.g.a@gmail.com>
2023-08-15 23:15:59 -0600

    array methods .contact

---
+++ 12-ArrayMethods/js/07-app.js
@@ -0,0 +1,7 @@
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio'];
+const meses2 = ['Agosto', 'Septiembre']
+const meses3 = ['Octubre', 'Noviembre']
+const meses4 = ['Diciembre']
+
+const totalMeses = meses.concat(meses2, meses3, meses4)
+console.log(totalMeses)
