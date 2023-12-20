651cbf23786881f7e5a166e0cf1ec3907c091e8c
Glenn Galicia <glenn.f.g.a@gmail.com>
2023-12-19 19:49:40 -0600

    Objectos - accesando a valores de un objeto

---
+++ 07-Objetos/js/02-app.js
@@ -0,0 +1,11 @@
+// Accesando a valores de un objeto
+
+const producto = {
+    nombre: "MacBook Pro 14",
+    precio: 65000,
+    disponible: true
+}
+
+console.log(producto)
+console.log(producto.nombre)
+console.log(producto['disponible'])
