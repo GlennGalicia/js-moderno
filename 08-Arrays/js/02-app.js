66c1675030a7c7fd70a88f5065a5062836ca8b7d
Glenn Galicia <glenn.f.g.a@gmail.com>
2023-12-21 23:57:11 -0600

    Arrays - acceso a valores de un arreglo

---
+++ 08-Arrays/js/02-app.js
@@ -0,0 +1,10 @@
+// Accesar a valores de un arreglo
+
+const numeros = [10, 20, 30, 40, 50]
+
+console.table(numeros)
+
+// accesar a un valor
+console.log(numeros[1])
+console.log(numeros[5])
+console.log(numeros[4])
