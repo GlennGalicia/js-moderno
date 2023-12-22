d46246df120f96ca7213021a71488dd1b725e2f9
Glenn Galicia <glenn.f.g.a@gmail.com>
2023-12-21 23:36:42 -0600

    Objetos - keys, values y entries

---
+++ 07-Objetos/js/13-app.js
@@ -0,0 +1,11 @@
+// Object keys, values y entries
+
+const producto = {
+    nombre: "MacBook Pro 14",
+    precio: 65000,
+    disponible: true
+}
+
+console.log(Object.keys(producto))
+console.log(Object.values(producto))
+console.log(Object.entries(producto))
