09e8ea4001b481e8371a9a535cd2bd1002361e07
Glenn Galicia <glenn.f.g.a@gmail.com>
2023-12-22 00:57:28 -0600

    Arrays - map para iterar un arreglo

---
+++ 08-Arrays/js/10-app.js
@@ -0,0 +1,19 @@
+// map para iterar un arrary
+
+const carrito = [
+    { nombre: "Monitor 4k", precio: 5800 },
+    { nombre: "Teclado Apple", precio: 4800 },
+    { nombre: "TrackPad Apple", precio: 2800 },
+    { nombre: "Logitech THX", precio: 3800 },
+    { nombre: "MacBook Pro 14", precio: 65000 },
+]
+
+carrito.forEach( producto => {
+    console.log(`${producto.nombre} - ${producto.precio}`)
+});
+
+const nuevoArray = carrito.map( producto => {
+    return `${producto.nombre} - ${producto.precio}`
+});
+
+console.log(nuevoArray)
