79f9fd1792456597ef0faf5a72359118f0c1a3eb
Glenn Galicia <glenn.f.g.a@gmail.com>
2023-12-21 23:50:46 -0600

    Arrays - creando arrays en JS

---
+++ 08-Arrays/js/01-app.js
@@ -0,0 +1,12 @@
+// Crear arrays en JS
+
+const numeros = [10, 20, 30]
+const meses = new Array('Enero', 'Febrero')
+
+console.log(numeros)
+console.log(meses)
+
+// arreglo con diferentes tipos de datos
+
+const deTodo = ['Hola', 10, true, { nombre: 'Glenn', apellido: 'Galicia' }, [1, 2, 3]]
+console.log(deTodo)
