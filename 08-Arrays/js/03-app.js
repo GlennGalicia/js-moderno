81b26b908dac26349570d2fe6e59413318f17170
Glenn Galicia <glenn.f.g.a@gmail.com>
2023-12-22 00:07:16 -0600

    Arrays - recorriendo un arreglo

---
+++ 08-Arrays/js/03-app.js
@@ -0,0 +1,13 @@
+// Recorrer un arreglo
+
+const meses = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo']
+
+console.table(meses)
+
+// cuanto mide el arreglo
+console.log(meses.length)
+
+// recorrer arreglo
+for (let i = 0; i < meses.length; i++) {
+    console.log(meses[i])
+}                    
