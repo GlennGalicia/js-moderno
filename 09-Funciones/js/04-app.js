4709a74e855672fc680a244dfe12eddca65609ad
Glenn Galicia <glenn.f.g.a@gmail.com>
2023-12-23 23:37:21 -0600

    Funciones - diferencia entre funciones y metodos

---
+++ 09-Funciones/js/04-app.js
@@ -0,0 +1,7 @@
+// Diferencia entre funciones y metodos
+
+const numero1 = 20
+const nuemero2 = '40'
+
+console.log(parseInt(nuemero2)); // Esto es una funcion
+console.log(numero1.toString()); // Esto es un método
