95de4850b028e0831e832f16ebf9f2eccb8feab6
Glenn Galicia <glenn.f.g.a@gmail.com>
2023-12-23 23:44:57 -0600

    Funciones - diferencia entre funciones y metodos

---
+++ 09-Funciones/js/05-app.js
@@ -0,0 +1,14 @@
+// Diferencias entre parametros y argumentos
+
+function sumar(a, b) { // parametros
+    console.log(a + b);
+}
+
+sumar(10, 3) // argumentos
+sumar(300, 5048)
+
+function saludar(nombre, apellido) {
+    console.log(`Hola ${nombre} ${apellido}, bienvenido`);
+}
+
+saludar('Glenn', 'Galicia')
