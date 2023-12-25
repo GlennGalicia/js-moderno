ea15f004d58a345e43cdcb4fdff0d6307c64d3ba
Glenn Galicia <glenn.f.g.a@gmail.com>
2023-12-24 23:45:55 -0600

    DOM - accesando a elementos de HTML con document

---
+++ 13-DOM/js/01-scripts.js
@@ -0,0 +1,20 @@
+// Interactuando con el DOM
+
+let elemento;
+
+//elemento = document
+//elemento = document.all
+elemento = document.head
+elemento = document.body
+elemento = document.forms[0]
+elemento = document.forms[0].id
+elemento = document.forms[0].method
+elemento = document.forms[0].classList
+
+elemento = document.links
+elemento = document.links[4]
+elemento = document.links[4].classList
+
+elemento = document.images
+
+console.log(elemento);
