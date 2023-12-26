30e12766595f9f444e1120596ce5b56da1475e39
Glenn Galicia <glenn.f.g.a@gmail.com>
2023-12-25 21:30:48 -0600

    DOM - querySelector

---
+++ 13-DOM/js/04-scripts.js
@@ -0,0 +1,20 @@
+// Seleccionar elemento por querySelector
+
+const card = document.querySelector('.card')
+console.log(card);
+
+// Selectores especificos como en CSS
+const info = document.querySelector('.premium .info')
+console.log(info);
+
+// Seleccionar el segundo card de Hospedaje
+const segundoCard = document.querySelector('section.hospedaje .card:nth-child(2)')
+console.log(segundoCard);
+
+// Seleccionar formulario
+const form = document.querySelector('#formulario')
+console.log(form);
+
+// Seleccionar elementos HTML
+const nav = document.querySelector('nav')
+console.log(nav);
