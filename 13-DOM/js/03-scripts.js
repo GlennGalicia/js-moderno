43b5e3c06d2c52d2b2e32064658ff32fee792337
Glenn Galicia <glenn.f.g.a@gmail.com>
2023-12-25 21:14:34 -0600

    DOM - getElementById

---
+++ 13-DOM/js/03-scripts.js
@@ -0,0 +1,7 @@
+// Seleccionar elemento por su atributo id
+
+const formulario = document.getElementById('formulario')
+console.log(formulario);
+
+const input = document.getElementById('btn-submit')
+console.log(input);
