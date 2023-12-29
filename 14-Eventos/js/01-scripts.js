04c205c5158e1a39266b8ef25b3d7f2df37bad91
Glenn Galicia <glenn.f.g.a@gmail.com>
2023-12-28 20:51:02 -0600

    Eventos - detectar cuando el HTML esta listo

---
+++ 14-Eventos/js/01-scripts.js
@@ -0,0 +1,10 @@
+// Detectar cuando el HTML esta listo
+
+console.log(1);
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log(2);
+    console.log('Documento Listo');
+})
+
+console.log(3);
