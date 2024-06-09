d9f5899e944fabd6951c9459f612cba1358f975b
Glenn Galicia <glennfernando.galicia.avina@bbva.com>
2024-06-08 22:51:33 -0600

    feat: Add form input fields and console log for user data

---
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Campos Datos Usuario
+    const inputEmail = document.querySelector('#email');
+    const inputAsunto = document.querySelector('#asunto');
+    const inputMensaje = document.querySelector('#mensaje');
+
+    console.log(inputEmail)
+    console.log(inputAsunto)
+    console.log(inputMensaje)
+})
