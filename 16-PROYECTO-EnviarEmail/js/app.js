ee024950e296e8226c11c91d12103ef40bb6c40e
Glenn Galicia <glennfernando.galicia.avina@bbva.com>
2024-06-19 21:37:55 -0600

    refactor: Update form submission to show spinner while sending email

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -13,15 +13,13 @@ document.addEventListener('DOMContentLoaded', function () {
     const formulario = document.querySelector('#formulario')
     const btnSubmit = document.querySelector('#formulario button[type="submit"]')
     const btnReset = document.querySelector('#formulario button[type="reset"]')
-
-    console.log(inputEmail)
-    console.log(inputAsunto)
-    console.log(inputMensaje)
+    const spinner = document.querySelector('#spinner')
 
     // Asignar Eventos a los inputs
     inputEmail.addEventListener('input', validar)
     inputAsunto.addEventListener('input', validar)
     inputMensaje.addEventListener('input', validar)
+    formulario.addEventListener('submit', enviarEmail)
 
     // Resetear formulario
     btnReset.addEventListener('click', e => {
@@ -93,5 +91,11 @@ document.addEventListener('DOMContentLoaded', function () {
         }
         btnSubmit.classList.remove('opacity-50')
         btnSubmit.disabled = false
+    }
+
+    function enviarEmail(e) {
+        e.preventDefault()
+        spinner.classList.add('flex')
+        spinner.classList.remove('hidden')
     }
 })
