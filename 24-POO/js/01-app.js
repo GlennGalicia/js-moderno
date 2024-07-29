5f58d08ad27e402cce0959fa7f4a0a175ac6aa15
Glenn Fernando Galicia Aviña <glenn.f.g.a@gmail.com>
2024-07-29 16:44:01 -0600

    24 POO - define and instance a new class

---
+++ 24-POO/js/01-app.js
@@ -0,0 +1,11 @@
+class Cliente {
+
+    constructor(nombre, saldo) {
+        this.nombre = nombre
+        this.saldo = saldo
+    }
+    
+}
+
+const glenn = new Cliente('Glenn', 54000)
+console.log(glenn);
